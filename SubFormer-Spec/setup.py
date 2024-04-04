ba7b92cf50bf1fc6fa35b08218e71b9cc2f88230
Zihan Pengmei <89473963+zpengmei@users.noreply.github.com>
2024-04-03 20:01:59 -0500

    Use the SubFormer-Spec folder

---
+++ SubFormer-Spec/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SubFormer-Spec',
+    version='0.01',
+    packages=[''],
+    url='',
+    license='',
+    author='Zihan Pengmei',
+    author_email='zpengmei@uchicago.edu',
+    description='Implementation of SubFormer with the Spectral token for molecular property prediction.'
+)
